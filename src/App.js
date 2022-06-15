8381b46018929bfd1f989541ad6124d0fd0fd100
aarontorresweb <aarontorresweb@gmail.com>
2022-06-14 22:14:25 -0700

    Nucamp site built with React - in the middle of State Exercise (campsitesSlice)

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import CampsitesList from './features/campsites/CampsitesList';
+import { CAMPSITES } from './app/shared/CAMPSITES';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CampsitesList/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
