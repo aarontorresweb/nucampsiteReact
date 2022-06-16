3495de34ba8ab16eb903dfa140f712268424fa36
aarontorresweb <aarontorresweb@gmail.com>
2022-06-15 22:30:51 -0700

    Exercise: useState() Part 1

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,18 +1,15 @@
+import { useState} from 'react';
 import { Container, Row, Col, Button } from 'reactstrap';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CampsitesList from '../features/campsites/CampsitesList';
 import { selectRandomCampsite } from '../features/campsites/CampsitesSlice';
 
 const CampsitesDirectoryPage = () => {
-    let selectedCampsite = selectRandomCampsite();
+    const [selectedCampsite, toggleCampsite] = useState(selectRandomCampsite());
 
-    const toggleCampsite = () => {
-        selectedCampsite = selectRandomCampsite();
-        console.log(selectedCampsite);
-    };
     return(
         <Container>
-            <Button onClick={() => toggleCampsite()}>
+            <Button onClick={() => toggleCampsite(selectRandomCampsite())}>
                 Select Random Campsite
             </Button>
             <Row>
