36c28cd9d312892f60883cf0bd55311a059130ce
aarontorresweb <aarontorresweb@gmail.com>
2022-06-21 15:22:38 -0700

    Week 2 Workshop with Task 4 Fixed

--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -1,27 +1,38 @@
-// import { Col } from 'reactstrap';
-// import Partner from '../../components/Partner.js';
-// import { selectAllPartners } from '../partners/PartnersSlice';
+import { Col } from 'reactstrap';
+import Partner from '../../components/Partner.js';
+import { selectAllPartners } from '../partners/PartnersSlice';
 
-// const PartnersList = () => {
-//     const partners = selectAllPartners();
-//     console.log(partners);
-//     // console.log(partner.id);
+const PartnersList = () => {
+    const partners = selectAllPartners();
+    console.log(partners);
+    console.log(partners[2].id);
 
-//     return (
-//         <Col className='mt-4'>
-//             {
-//                 partners.map(partner => (
-//                     {
-//                         return(
-//                             <div className='d-flex mb-5' key={partner.id}>
-//                                 <Partner partner={partner}/>
-//                             </div>
-//                         )
-//                     }
-//                 );
-//             }
-//         </Col>
-//     );
-// }
+    return (
+        <Col className='mt-4'>
+            {
+                partners.map(partner => {
+                    return(
+                        <div className='d-flex mb-5' key={partner.id}>
+                            <Partner partner={partner}/>
+                        </div>
+                    )
+                })
+            }
+        </Col>
+    );
+}
 
-// export default PartnersList;
+        // // This is the broken function that I had trouble with. 
+        // // I left it in just for comparison and perhaps understanding later what is wrong with it.
+        // {
+        //     partners.map(partner =>
+        //          {
+        //              return(
+        //                 <div className='d-flex mb-5' key={partner.id}>
+        //                      <Partner partner={partner}/>
+        //                  </div>
+        //              )
+        //          }
+        //  };
+
+export default PartnersList;
