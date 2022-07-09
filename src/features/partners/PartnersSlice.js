156c77f97ac5d8e12f2582331b4d13a2d73ef70f
aarontorresweb <aarontorresweb@gmail.com>
2022-07-09 01:33:53 -0700

    Fetch and Async Redux Practice 3. Exercise: Async DisplayList

--- src/features/partners/PartnersSlice.js
+++ src/features/partners/PartnersSlice.js
@@ -32,7 +32,7 @@ const partnersSlice = createSlice({
         [fetchPartners.fulfilled]: (state, action) => {
             state.isLoading = false;
             state.errMsg = '';
-            state.PartnersArray = mapImageURL(action.payload);
+            state.partnersArray = mapImageURL(action.payload);
         },
         [fetchPartners.rejected]: (state, action) => {
             state.isLoading = false;
